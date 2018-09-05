9ae30fc61fce241e46385586a1be90bdb0f2cc62
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2018-09-05 18:52:56 +0200

    basically functioning

---
+++ HeapProblemVisualizer/Assets/Scripts/gridSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridSpawner : MonoBehaviour {
+
+	public int length = 0;
+	public GameObject PointPrefab;
+
+	void SpawnGrid ()
+	{
+		for (int x = 0; x < length; x++)
+		{
+			for (int y = 0; y < length; y++)
+			{
+				for (int z = 0; z < length; z++)
+				{
+					Instantiate(PointPrefab, new Vector3(x, y, z), Quaternion.identity);
+				}
+			}
+		}
+	}
+
+	void Start () 
+	{
+		SpawnGrid();
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
---
+++ HeapProblemVisualizer/Assets/Scripts/pointController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointController : MonoBehaviour {
+
+	public bool isWinningPosition = true;
+
+	private GameObject[] Points;
+
+	void Start () 
+	{
+		Points = GameObject.FindGameObjectsWithTag("Point");
+
+		foreach (GameObject P in Points)
+		{
+			if (P.transform.position.x == this.transform.position.x && P.transform.position.y == this.transform.position.y && P.transform.position.z < this.transform.position.z && P.GetComponent<pointController>().isWinningPosition)
+			{
+				isWinningPosition = false;
+			}
+
+			if (P.transform.position.x == this.transform.position.x && P.transform.position.z == this.transform.position.z && P.transform.position.y < this.transform.position.y && P.GetComponent<pointController>().isWinningPosition)
+			{
+				isWinningPosition = false;
+			}
+
+			if (P.transform.position.y == this.transform.position.y && P.transform.position.z == this.transform.position.z && P.transform.position.x < this.transform.position.x && P.GetComponent<pointController>().isWinningPosition)
+			{
+				isWinningPosition = false;
+			}
+		}
+
+		if (isWinningPosition)
+		{
+			this.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+		}
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.T) && !isWinningPosition)
+		{
+			this.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 0);
+		}
+	}
+}
